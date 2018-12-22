9bdcd791194bdd49ba2c3ed052c2714acb4ab5fb
jiho617 <45708009+jiho617@users.noreply.github.com>
2018-12-22 16:57:26 +0900

    ch04

---
+++ Ch04/Ex06.py
@@ -0,0 +1,4 @@
+numlist = [1, 2, 3, 4]
+print("리스트=", numlist)
+sumNum = numlist[0] + numlist[1] + numlist[2] + numlist[3]
+print("리스트 숫자의 합 =", sumNum)
---
+++ Ch04/lab1.py
@@ -0,0 +1,17 @@
+friend_list=[]
+a = input("친구을 이름을 입력하세요")
+friend_list.append(a)
+print(friend_list)
+a = input("친구을 이름을 입력하세요")
+friend_list.append(a)
+print(friend_list)
+a = input("친구을 이름을 입력하세요")
+friend_list.append(a)
+print(friend_list)
+a = input("친구을 이름을 입력하세요")
+friend_list.append(a)
+print(friend_list)
+a = input("친구을 이름을 입력하세요")
+friend_list.append(a)
+print(friend_list)
+
